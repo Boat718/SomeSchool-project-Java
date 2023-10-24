34b92cbb8d1f67039049c933785353fe3f9519a8
boat718 <boat_loading@hotmail.com>
2023-10-24 20:21:54 +0700

    add authentication and use password hashing

--- src/main/java/com/project/someschool/service/PersonService.java
+++ src/main/java/com/project/someschool/service/PersonService.java
@@ -7,6 +7,7 @@ import com.project.someschool.model.Roles;
 import com.project.someschool.repository.PersonRepository;
 import com.project.someschool.repository.RolesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +19,14 @@ public class PersonService {
     @Autowired
     private RolesRepository rolesRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public boolean createNewPerson(Person person){
         boolean isSaved = false;
         Roles role = rolesRepository.getByRoleName(SomeSchoolConstants.STUDENT_ROLE);
         person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
         person = personRepository.save(person);
         if (null != person && person.getPersonId() > 0)
         {
